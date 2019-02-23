d325ddde0a1d722f1cba93a0eca570ceaaaf048a
Doom <clement.doumergue@epitech.eu>
2019-02-23 00:58:30 -0500

    Add std::rbegin/std::rend and comparison operators for std::reverse_iterator

--- tests/tests-iterator.cpp
+++ tests/tests-iterator.cpp
@@ -16,8 +16,24 @@ ut_test(iterator_traits_pointer)
     static_assert(std::is_same_v<typename ptr_traits::iterator_category, std::random_access_iterator_tag>);
 }
 
+ut_test(reverse_iterator)
+{
+    int arr1[] = {1, 2, 3, 4, 5};
+    int arr2[] = {5, 4, 3, 2, 1};
+
+    auto it1 = std::begin(arr1);
+    auto rit2 = std::rbegin(arr2);
+    while (it1 != std::end(arr1)) {
+        ut_assert_eq(*it1, *rit2);
+        ++it1;
+        ++rit2;
+    }
+    ut_assert_eq(rit2, std::rend(arr2));
+}
+
 ut_group(iterator,
-         ut_get_test(iterator_traits_pointer)
+         ut_get_test(iterator_traits_pointer),
+         ut_get_test(reverse_iterator)
 );
 
 void run_iterator_tests()
